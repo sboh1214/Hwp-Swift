bf0ddb17ff060d2a594eeee5a6ce26c71185a3d8
Seungbin Oh <sboh1214@gmail.com>
2020-09-19 13:00:45 +0900

    run test for all branches

--- Package.swift
+++ Package.swift
@@ -14,7 +14,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/CoreOffice/OLEKit.git", .revision("6a809b91a9a524594c02d990dbce157d7e9cb447"))
+        .package(
+            url: "https://github.com/CoreOffice/OLEKit.git",
+            .revision("6a809b91a9a524594c02d990dbce157d7e9cb447")
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
