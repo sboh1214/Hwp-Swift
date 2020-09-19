d4ae16fefd9476e401c29c4a893b6ba76598b557
Seungbin Oh <sboh1214@gmail.com>
2020-09-19 15:23:27 +0900

    add 2007 hwp file

--- Tests/HwpKitTests/HwpKitTests.swift
+++ Tests/HwpKitTests/HwpKitTests.swift
@@ -5,21 +5,21 @@ final class HwpKitTests: XCTestCase {
     func openHwp() throws -> HwpFile {
         let url = URL(fileURLWithPath: #file)
             .deletingLastPathComponent()
-            .appendingPathComponent("SampleHwp")
+            .appendingPathComponent("Versions")
             .appendingPathComponent("blank.hwp")
         return try HwpFile(filePath: url.path)
     }
-
+    
     func testOpen() throws {
         let hwp = try openHwp()
         XCTAssertNotNil(hwp.fileHeader)
     }
-
+    
     func testSignature() throws {
         let hwp = try openHwp()
         XCTAssertEqual(hwp.fileHeader.signature, "HWP Document File\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
     }
-
+    
     func testHwpVersion() throws {
         let hwp = try openHwp()
         XCTAssertEqual(hwp.fileHeader.version.major, 5)
@@ -27,12 +27,12 @@ final class HwpKitTests: XCTestCase {
         XCTAssertEqual(hwp.fileHeader.version.build, 3)
         XCTAssertEqual(hwp.fileHeader.version.revision, 4)
     }
-
+    
     func testEncryptVersion() throws {
         let hwp = try openHwp()
         XCTAssertEqual(hwp.fileHeader.encryptVersion, 4)
     }
-
+    
     static var allTests = [
         ("testOpen", testOpen),
         ("testSignature", testSignature),
