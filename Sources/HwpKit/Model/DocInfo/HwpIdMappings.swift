9bf821107e1e62d41ead2a6d31f966a941a16b7d
Seungbin Oh <sboh1214@gmail.com>
2020-10-09 17:36:49 +0900

    parse id mappings

--- Sources/HwpKit/Model/DocInfo/HwpIdMappings.swift
+++ Sources/HwpKit/Model/DocInfo/HwpIdMappings.swift
@@ -1,7 +1,46 @@
 import Foundation
 
+/**
+ 아이디 매핑 헤더
+ 
+ Tag ID : HWPTAG_ID_MAPPINGS
+ */
 public struct HwpIdMappings: HwpFromRecordWithVersion {
-    
+    /**바이너리 데이터*/
+    private let binaryDataCount : Int32
+    /**영어 글꼴*/
+    private let englishFaceCount: Int32
+    /**한자 글꼴*/
+    private let chineseFaceCount: Int32
+    /**일어 글꼴*/
+    private let japaneseFaceCount: Int32
+    /**기타 글꼴*/
+    private let etcFaceCount: Int32
+    /**기호 글꼴*/
+    private let symbolFaceCount: Int32
+    /**사용자 글꼴*/
+    private let userFaceCount: Int32
+    /**테두리/배경*/
+    private let borderFillCount: Int32
+    /**글자 모양*/
+    private let charShapeCount: Int32
+    /**탭 정의*/
+    private let tabDefCount: Int32
+    /**문단 번호*/
+    private let paraNumberingCount: Int32
+    /**글머리표*/
+    private let paraHeadCount: Int32
+    /**문단 모양*/
+    private let paraShapeCount: Int32
+    /**스타일*/
+    private let styleCount: Int32
+    /**메모 모양 (5.0.2.1 이상)*/
+    private var memoShapeCount: Int32? = nil
+    /**변경추적 (5.0.3.2 이상)*/
+    private var changeTraceCount: Int32? = nil
+    /**변경추적 사용자 (5.0.3.2 이상)*/
+    private var changeTraceUserCount: Int32? = nil
+
     public var binDataArray: [HwpBinData]
     public var faceNameArray: [HwpFaceName]
     public var borderFillArray: [HwpBorderFill]
@@ -12,6 +51,32 @@ public struct HwpIdMappings: HwpFromRecordWithVersion {
     public var paraShapeArray: [HwpParaShape]
     
     init(_ record: HwpRecord, _ version: HwpVersion) throws {
+        var reader = DataReader(record.payload)
+        defer {
+            // precondition(reader.isEOF())
+        }
+        binaryDataCount = reader.read(Int32.self)
+        englishFaceCount = reader.read(Int32.self)
+        chineseFaceCount = reader.read(Int32.self)
+        japaneseFaceCount = reader.read(Int32.self)
+        etcFaceCount = reader.read(Int32.self)
+        symbolFaceCount = reader.read(Int32.self)
+        userFaceCount = reader.read(Int32.self)
+        borderFillCount = reader.read(Int32.self)
+        charShapeCount = reader.read(Int32.self)
+        tabDefCount = reader.read(Int32.self)
+        paraNumberingCount = reader.read(Int32.self)
+        paraHeadCount = reader.read(Int32.self)
+        paraShapeCount = reader.read(Int32.self)
+        styleCount = reader.read(Int32.self)
+        if version >= HwpVersion(5, 0, 2, 1) {
+            memoShapeCount = reader.read(Int32.self)
+        }
+        if version >= HwpVersion(5,0,3,2) {
+            changeTraceCount = reader.read(Int32.self)
+            changeTraceUserCount = reader.read(Int32.self)
+        }
+        
         binDataArray = try record.children
             .filter {$0.tagId == HwpDocInfoTag.binData}
             .map {try HwpBinData($0.payload)}
