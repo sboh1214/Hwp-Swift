8e43ef1f4063285b9580413bd98eecd6b76ec478
Seungbin Oh <sboh1214@gmail.com>
2020-10-10 16:45:31 +0900

    make HwpFile conforms HwpPrimitive

---
+++ Tests/HwpKitTests/Blank/CreateTests.swift
@@ -0,0 +1,17 @@
+import HwpKit
+import XCTest
+
+final class CreateTests: XCTestCase {
+    func createHwp() throws -> (HwpFile, HwpFile) {
+        let url = URL(fileURLWithPath: #file)
+            .deletingLastPathComponent()
+            .appendingPathComponent("blank.hwp")
+        let expected =  try HwpFile(filePath: url.path)
+        let actual = HwpFile()
+        return (expected, actual)
+    }
+
+    func testCreate() throws {
+        let (expected, actual) = try createHwp()
+    }
+}
