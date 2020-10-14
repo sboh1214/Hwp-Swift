2f014f55d8bb0090bb8114d36cc7795c27ea8de2
Seungbin Oh <sboh1214@gmail.com>
2020-10-14 20:30:00 +0900

    refactor HwpVersion

--- Tests/HwpKitFrameworkTests/Versions/VersionTests.swift
+++ Tests/HwpKitFrameworkTests/Versions/VersionTests.swift
@@ -4,18 +4,12 @@ import XCTest
 final class VersionTests: XCTestCase {
     func test2007() throws {
         let hwp = openHwp(#file, "2007")
-        XCTAssertEqual(hwp.fileHeader.version.major, 5)
-        XCTAssertEqual(hwp.fileHeader.version.minor, 0)
-        XCTAssertEqual(hwp.fileHeader.version.build, 2)
-        XCTAssertEqual(hwp.fileHeader.version.revision, 2)
+        XCTAssertEqual(hwp.fileHeader.version, HwpVersion(5, 0, 2, 2))
     }
 
     func test2014VP() throws {
         let hwp = openHwp(#file, "2014VP")
-        XCTAssertEqual(hwp.fileHeader.version.major, 5)
-        XCTAssertEqual(hwp.fileHeader.version.minor, 0)
-        XCTAssertEqual(hwp.fileHeader.version.build, 5)
-        XCTAssertEqual(hwp.fileHeader.version.revision, 0)
+        XCTAssertEqual(hwp.fileHeader.version, HwpVersion(5, 0, 5, 0))
     }
 
     static var allTests = [
