42a0b3f27927cfce3b0736f291cebeb9bf991027
Seungbin Oh <sboh1214@gmail.com>
2021-02-27 16:01:04 +0900

    Add more tests for HwpColumn(Property)

--- Tests/CoreHwpTests/Section/Column/ColumnTests.swift
+++ Tests/CoreHwpTests/Section/Column/ColumnTests.swift
@@ -5,19 +5,40 @@ import XCTest
 final class ColumnTests: XCTestCase {
     func testColumn() throws {
         let hwp = try openHwp(#file, "Column")
-        guard let ctrlArray = hwp.sectionArray[0].paragraph[0].ctrlHeaderArray else {
-            XCTFail()
-            return
-        }
-        let columnArray: [HwpColumn] = ctrlArray.compactMap {
-            switch $0 {
-            case let .column(hwpColumn):
-                return hwpColumn
-            default:
-                return nil
-            }
+        let paragraphArray = hwp.sectionArray[0].paragraph
+
+        let columnArray: [HwpColumn] = paragraphArray.map {
+            $0.ctrlHeaderArray!.compactMap {
+                switch $0 {
+                case let .column(hwpColumn):
+                    return hwpColumn
+                default:
+                    return nil
+                }
+            }[0]
         }
 
-        print(columnArray)
+        expect(columnArray[0].property.count) == 1
+        expect(columnArray[1].property.count) == 2
+        expect(columnArray[2].property.count) == 3
+        expect(columnArray[3].property.count) == 2
+        expect(columnArray[4].property.count) == 2
+
+        expect(columnArray[0].property.isSameWidth) == true
+        expect(columnArray[1].property.isSameWidth) == true
+        expect(columnArray[2].property.isSameWidth) == true
+        expect(columnArray[3].property.isSameWidth) == false
+        expect(columnArray[4].property.isSameWidth) == false
+
+        expect(columnArray[0].property.direction) == .left
+
+        expect(columnArray[0].widthArray).to(beNil())
+        expect(columnArray[1].widthArray).to(beNil())
+        expect(columnArray[2].widthArray).to(beNil())
+
+        expect(columnArray[3].widthArray?[0]) == 0
+        expect(columnArray[3].widthArray?[1]) == 10339
+        expect(columnArray[4].widthArray?[0]) == 0
+        expect(columnArray[4].widthArray?[1]) == 20680
     }
 }
